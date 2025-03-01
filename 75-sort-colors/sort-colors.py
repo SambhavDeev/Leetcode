8942b02b192900088d3ee5615444711961e167b2
Sambhav Dev <165070964+SambhavDeev@users.noreply.github.com>
2025-03-02 01:23:53 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (12.12%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,4 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        return nums.sort()
+        #using dutch national flag
+        n=len(nums)
+        low,mid,high=0,0,n-1
+
+        while mid<=high:
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                mid+=1
+                low+=1
+
+            elif nums[mid]==1:
+                mid+=1
+
+            else:#nums[mid]==2
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+
+        return nums
