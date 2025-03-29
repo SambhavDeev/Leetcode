d8ac433ee2f4b721afc16e7c8ed242b4905a280c
Sambhav Dev <165070964+SambhavDeev@users.noreply.github.com>
2025-03-30 04:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (25.58%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,21 +1,29 @@
-class Solution:
-    def bs(self,mat,target):
-        low=0
-        high=len(mat)
-        while low<=high:
-            mid=(low+high)//2
-            if mat[mid]==target:
+class Solution(object):
+    def bs(self, arr,target):
+        l=0
+        r=len(arr)-1
+        while l<=r:
+            mid=(l+r)//2
+            if arr[mid]==target:
                 return True
-            elif mat[mid]<target:
-                low=mid+1
-            else:
-                high=mid-1
-        return False
+            elif arr[mid]<target:
+                l=mid+1
 
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        r,c=len(matrix),len(matrix[0])
+            else:
+                r=mid-1
+        return False
+        
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        r=len(matrix)
+        c=(len(matrix[0])-1)
         for i in range(r):
-            if matrix[i][0]<=target<=matrix[i][c-1]:
+            if matrix[i][0]<=target<=matrix[i][c]:
                 return self.bs(matrix[i],target)
         
         return False
+        
