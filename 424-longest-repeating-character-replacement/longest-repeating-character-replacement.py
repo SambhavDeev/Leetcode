a0faf4934acb89e03c3d9d17b92c67eb7b9cd8f3
Sambhav Dev <165070964+SambhavDeev@users.noreply.github.com>
2025-04-01 08:36:15 +0530

    Time: 228 ms (16.58%) | Memory: 17.8 MB (99.61%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,21 +1,18 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int: #sliding window
         l=0
-        char=[0]*26
         freq=0
+        char=[0]*26
         for r in range(len(s)):
-            char[ord(s[r])-ord('A')]+=1
-            while (r-l+1)- max(char) >k:
-                char[ord(s[l])- ord('A')]-=1
+            char[ord(s[r])-ord("A")]+=1
+            while (r-l+1)-max(char)>k:
+                char[ord(s[l])-ord("A")]-=1
                 l+=1
-            freq=max(freq,r-l+1)
+            freq=max(freq,(r-l+1))
 
         return freq
 
-            
+
+      
+     
         
