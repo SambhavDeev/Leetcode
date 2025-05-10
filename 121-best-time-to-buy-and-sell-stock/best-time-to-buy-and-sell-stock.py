96974c30dc4c889fb32f48b461e18bd9c908df39
Sambhav Dev <165070964+SambhavDeev@users.noreply.github.com>
2025-05-11 02:44:09 +0530

    Time: 99 ms (48.31%) | Memory: 26.9 MB (33.30%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,12 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:#slinding window
-        maxp=0
-        l,r=0,1
+    def maxProfit(self, prices: List[int]) -> int:#sliding window
+        maxer=0
+        l=0
+        r=1
         while r<len(prices):
             if prices[l]<prices[r]:
-                maxp=max(maxp,(prices[r]-prices[l]))
+                maxer=max(maxer,(prices[r]-prices[l]))
             else:
                 l=r
             r+=1
-        return maxp
+        return maxer
