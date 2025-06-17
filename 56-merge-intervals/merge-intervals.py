10efc626a5ebc540e8963b015a69bfc7144605cd
Sambhav Dev <165070964+SambhavDeev@users.noreply.github.com>
2025-06-17 14:22:28 +0530

    Time: 7 ms (72.74%) | Memory: 20.9 MB (89.54%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,13 +1,11 @@
 class Solution:
     def merge(self, arr: List[List[int]]) -> List[List[int]]:
         ans=[]
-        n=len(arr)
         arr.sort()
-        for i in range(n):
+        for i in range(len(arr)):
             if not ans or arr[i][0]>ans[-1][1]:
                 ans.append(arr[i])
-
             else:
                 ans[-1][1]=max(arr[i][1],ans[-1][1])
-
-        return ans        
+        
+        return ans
